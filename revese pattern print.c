1816a13f5db952c9c4ccaa589fb78693fb15eff1
Tharanan <108599220+PTharanan@users.noreply.github.com>
2023-06-03 18:02:32 +0530

    Add files via upload

---
+++ revese pattern print.c
@@ -0,0 +1,25 @@
+/*
+ ============================================================================
+ Name        : revese.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int i, n, j;
+	printf("Enter last number: ");
+	scanf("%d", &n);
+	for (i = n; i >= n; i--) {
+		for (j = 0; j < i; j--) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return EXIT_SUCCESS;
+}
